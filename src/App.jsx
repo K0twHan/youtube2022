676286878afba44ba1576f8036526afc5ea94ace
Batuhan <batuhan.tekin.ceng@gmail.com>
2023-10-08 00:28:53 +0300

    İlk Gün

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import "./style.scss"
 function App() {
-  return <div>Hello World!</div>;
+  return(
+  <Home/>
+  );
 }
 
 export default App;
