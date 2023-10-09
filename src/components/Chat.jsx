590ae7fee7dc03cc614dc25673015d0258232c1d
Batuhan <batuhan.tekin.ceng@gmail.com>
2023-10-09 23:03:03 +0300

    2. gün

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Cam from '../images/cam.png'
+import Add from '../images/add.png'
+import More from '../images/more.png'
+import Messages from './Messages'
+import Input from './Input'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className='chatInfo'>
+      <span>Jane</span>
+      <div className="chatIcon">
+        <img src={Cam} alt="" />
+        <img src={Add} alt="" />
+        <img src={More} alt="" />
+
+      </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
 
